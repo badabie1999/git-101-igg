e970ba53577dd2b3d03b30738d3646e071673073
badabie1999 <71362446+badabie1999@users.noreply.github.com>
2020-09-16 13:47:02 +0700

    Create quiz.js

---
+++ js/quiz.js
@@ -0,0 +1,11 @@
+let person = {
+    name : 'Charida',
+    lastname : 'Sinphongphaisan',
+    age : 21,
+    nickname = 'Beammie',
+    hobby = 'listen music',
+    tel = 0882772909,
+
+
+}
+console.log(person)
