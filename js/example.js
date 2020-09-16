8b072418de4d4e126a5b6e9fd49fed4174f013be
badabie1999 <71362446+badabie1999@users.noreply.github.com>
2020-09-16 13:39:12 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,18 @@
+
+let name = "Anu"
+name = "BEAM"
+let test = "Hello"
+name = `${test} B`
+
+let arr = ['apple','banana','kiwi']
+console.log("arr : ", arr[1])
+
+let person = {
+    name : 'BEAM',
+    age : 15,
+    gender :  {
+        name : 'female'
+    },
+    arr
+}
+console.log(person)
