24995ff89fb62a009724ad8fd76b1a6cf69fc965
badabie1999 <71362446+badabie1999@users.noreply.github.com>
2020-09-16 15:00:55 +0700

    [add] quiz2

---
+++ js/quiz2.js
@@ -0,0 +1,17 @@
+for(let i = 1; i <= 20 ; i+=1) {
+    console.log(i)
+}
+
+for(let i = 1; i <= 20 ; i+=2) {
+    console.log(i)
+}
+
+let temp = [-1,,3,56,-93,0,4].map(el => el * 2 )
+console.log(temp)
+
+let temps = [18,43,14,,44,55,,89,0,12,1].filter(el => el % 2 )
+console.log(temps)
+
+
+
+
